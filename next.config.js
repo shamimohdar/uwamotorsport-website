9db5de11bbaff76298fdd7040d92a07ab236c153
shamimohdar <mohdarshami@gmail.com>
2023-08-15 11:50:05 +0800

    v1.7 included x-robot tag

--- next.config.js
+++ next.config.js
@@ -1,6 +1,20 @@
 const nextConfig = {
   reactStrictMode: true,
-
+  async headers() {
+    const headers = [];
+    if (process.env.NEXT_PUBLIC_VERCEL_ENV === 'preview') {
+      headers.push({
+        headers: [
+          {
+            key: 'X-Robots-Tag',
+            value: 'noindex',
+          },
+        ],
+        source: '/:path*',
+      });
+    }
+    return headers;
+  },
   webpack: (config) => {
     config.module.rules.push({
       test: /\.(glb)$/i,
